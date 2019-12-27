8dcbf5bef01057c06906e4092a60221845d43ab9
Seasnak <sasank1020@gmail.com>
2019-12-26 23:30:49 -0800

    Created a tkinter application

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-import Tkinter
+import os
+import tkinter as tk
 import pygame
+
+def callback(selection):
+    print(f"Selected \"{selection}\"")
+    return
+
+if __name__ == '__main__':
+    # print(os.environ.get('DISPLAY', ''))
+    top = tk.Tk() #creating tkinter window
+
+    sort_frame = tk.Frame(top)
+    sort_frame.pack()
+
+    sort_label = tk.Label(sort_frame, text = "Sorting Algorithm: ")
+    sort_label.pack( side = tk.LEFT )
+
+    sorting_algorithms = ["A*", "Djikstra\'s", "Bellman Ford", "Floyd-Warshall\'s"]
+    option = tk.StringVar(top)
+    option.set("A*") #set default option
+    callback(option.get())
+    #note you can get the current output with option.get()
+
+    sort_opt = tk.OptionMenu(sort_frame, option, *sorting_algorithms, command=callback)
+    sort_opt.pack( side = tk.LEFT )
+
+    top.mainloop() #run tkinter window
